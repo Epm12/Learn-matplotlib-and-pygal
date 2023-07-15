2a47a7a148e4fdf50f4d14f4eb1dc249ba6536c7
Epm12 <37344234+Epm12@users.noreply.github.com>
2023-07-15 22:05:26 +0200

    Add files via upload
    Used 
    - Render_to_file
    - render_in_browser

--- Pygal_library_tryou_in_Thonny/First_tonny_program.py
+++ Pygal_library_tryou_in_Thonny/First_tonny_program.py
@@ -2,5 +2,7 @@
 Simple example code for pygal, use to test installation
 """
 
+import lxml
 import pygal
 pygal.Bar()(1,3,5,6)(2,3,4,6).render_to_file("pygal_test.svg")
+pygal.Bar()(1,3,5,6)(2,3,4,6).render_in_browser()
